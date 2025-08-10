a882e3dde24b9afed01d7823dbf55cf80ef39488
HalavicH <HalavicH@gmail.com>
2025-08-10 21:31:26 +0200

    Add gamepad resource

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     info!("Starting...");
@@ -6,6 +8,23 @@ fn main() {
     let exit = App::new()
         // Bevy Plugins
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin::default())
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_systems(Startup, |mut commands: Commands| {
+            // Spawn a 2D camera
+            commands.spawn(Camera2d);
+        })
+        .add_systems(Update, list_gamepads)
         .run();
     info!("App exited with: {:?}", exit);
+}
+
+fn list_gamepads(gamepad: Query<(&Gamepad, &GamepadSettings, &Name)>) {
+    let Ok(gp) = gamepad.single() else {
+        println!("No gamepads connected.");
+        return;
+    };
+    let (g, gs, name) = gp;
+    println!("Currently connected gamepad: {name}");
+    println!("Gamepad: {g:#?}, Settings: {gs:#?}");
 }
---
+++ todo.md
@@ -0,0 +1,4 @@
+## TODO:
+- [x] Figure out input from the controller
+- [ ] Setup drone entity (cube)
+- [ ] Wire inputs from the controller to the drone to control its axis movements

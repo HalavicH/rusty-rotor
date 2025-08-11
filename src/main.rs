9674c2537f88df79d2657fbd64b7c0f8724eb96e
HalavicH <HalavicH@gmail.com>
2025-08-11 09:32:57 +0200

    feat: Add stick position visualization

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,108 @@ fn main() {
         .add_plugins(WorldInspectorPlugin::new())
         // Game resources
         // Game systems
-        .add_systems(Startup, (setup, setup_ui))
-        .add_systems(Update, (update_drone_controls, update_drone_controls_ui))
+        .add_systems(Startup, (setup, setup_ui, spawn_stick_position_ui))
+        .add_systems(
+            Update,
+            (
+                update_drone_controls,
+                update_drone_controls_ui,
+                update_stick_position,
+            ),
+        )
         .add_systems(Update, rotate_drone_system)
         // .add_systems(Update, list_gamepads)
         .run();
     info!("App exited with: {:?}", exit);
+}
+
+#[derive(Debug, Clone, Copy, Component)]
+enum StickSideUi {
+    Left,
+    Right,
+}
+
+/// This system spawns a UI node/2d sprites to display the stick position.
+fn spawn_stick_position_ui(
+    mut commands: Commands,
+    // Add other resources if needed
+) {
+    commands.spawn((
+        Node {
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::End,
+            column_gap: Val::Px(5.),
+            ..Default::default()
+        },
+        Text::new("Stick Position".to_string()),
+        Name::new("Gamepad Stick Position UI"),
+        children![
+            build_stick_ui(StickSideUi::Left),
+            build_stick_ui(StickSideUi::Right),
+        ],
+    ));
+}
+
+fn build_stick_ui(side: StickSideUi) -> impl Bundle {
+    (
+        Name::new(format!("{side:?}: Stick: Box")),
+        Text::new(format!("{side:?}: Stick: Box")),
+        Node {
+            width: Val::Px(200.),
+            height: Val::Px(200.),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..Default::default()
+        },
+        BackgroundColor(Color::Srgba(Srgba::new(0.1, 0.1, 0.1, 0.5))),
+        children![(
+            Name::new(format!("{side:?}: Stick: Dot")),
+            side,
+            Node {
+                width: Val::Percent(5.),
+                height: Val::Percent(5.),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            BackgroundColor(Color::Srgba(Srgba::new(0.5, 0.0, 0.0, 0.9))),
+            BorderRadius {
+                top_left: Val::Percent(50.),
+                top_right: Val::Percent(50.),
+                bottom_left: Val::Percent(50.),
+                bottom_right: Val::Percent(50.),
+            },
+        )],
+    )
+}
+
+fn update_stick_position(
+    controls: Query<&DronePosition, With<PlayerDrone>>,
+    mut query: Query<(&mut Node, &StickSideUi)>,
+) {
+    let Some(controls) = controls.single().ok() else {
+        info!("No drone controls found.");
+        return;
+    };
+
+    for (mut node, side) in query.iter_mut() {
+        match side {
+            StickSideUi::Left => {
+                // x-axis
+                node.left = Val::Percent(100. * controls.yaw / 2.);
+                // y-axis
+                node.bottom = Val::Percent((100. * controls.throttle) - 50.);
+            }
+            StickSideUi::Right => {
+                // x-axis
+                node.left = Val::Percent(100. * controls.roll / 2.);
+                // y-axis
+                node.bottom = Val::Percent(100. * controls.pitch / 2.);
+            }
+        }
+    }
 }
 
 #[derive(Resource)]
@@ -60,15 +156,15 @@ fn update_drone_controls_ui(
     };
     text.0 = format!(
         "Thrust: {:.2}\nPitch: {:.2}\nRoll: {:.2}\nYaw: {:.2}",
-        controls.thrust, controls.pitch, controls.roll, controls.yaw
+        controls.throttle, controls.pitch, controls.roll, controls.yaw
     );
 }
 #[derive(Debug, Default, Component)]
 pub struct DronePosition {
-    pub thrust: f32,
+    pub throttle: f32,
+    pub yaw: f32,
     pub pitch: f32,
     pub roll: f32,
-    pub yaw: f32,
 }
 
 fn update_drone_controls(
@@ -76,11 +172,11 @@ fn update_drone_controls(
     mut controls: Query<&mut DronePosition, With<PlayerDrone>>,
 ) {
     let Ok(gamepad) = gamepad.single() else {
-        info!("No gamepad found for drone controls.");
+        debug!("No gamepad found for drone controls.");
         return;
     };
     let Ok(mut controls) = controls.single_mut() else {
-        info!("No drone controls found.");
+        debug!("No drone controls found.");
         return;
     };
 
@@ -90,10 +186,10 @@ fn update_drone_controls(
         .get(GamepadInput::Button(GamepadButton::RightTrigger2))
         .unwrap_or(0.);
 
-    controls.thrust = right_trigger2;
-    controls.pitch = left_stick.y;
-    controls.roll = -left_stick.x;
-    controls.yaw = -right_stick.x;
+    controls.throttle = right_trigger2;
+    controls.yaw = right_stick.x;
+    controls.pitch = -left_stick.y;
+    controls.roll = left_stick.x;
 }
 
 fn rotate_drone_system(
@@ -101,26 +197,26 @@ fn rotate_drone_system(
     controls: Query<&DronePosition, With<PlayerDrone>>,
 ) {
     let Some(drone_controls) = controls.single().ok() else {
-        info!("No drone controls found.");
+        debug!("No drone controls found.");
         return;
     };
-    info!("{drone_controls:?}");
+    trace!("{drone_controls:?}");
     // Get the gamepad's left stick input.
     let DronePosition {
-        thrust,
+        throttle: thrust,
         pitch,
         roll,
         yaw,
     } = drone_controls;
 
     let Ok(mut transform) = drone.single_mut() else {
-        info!("No drone entity found.");
+        debug!("No drone entity found.");
         return;
     };
 
     let coef = 1.0;
     // Apply rotation based on the gamepad input.
-    transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw * coef, pitch * coef, roll * coef);
+    transform.rotation = Quat::from_euler(EulerRot::YXZ, -yaw * coef, pitch * coef, roll * coef);
     transform.translation.y = *thrust;
 }
 
@@ -145,7 +241,7 @@ fn setup(
     // Spawn a camera looking at the entities to show what's happening in this example.
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(2.0, 1.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(-2.0, 2.0, -5.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
     // Add a light source so we can see clearly.

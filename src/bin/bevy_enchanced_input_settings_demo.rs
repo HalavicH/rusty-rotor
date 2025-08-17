faa69a3e50652fa6f60bbc092b72eeb316fa044e
HalavicH <HalavicH@gmail.com>
2025-08-17 10:38:55 +0200

    feat: Add menu ui

--- src/bin/bevy_enchanced_input_settings_demo.rs
+++ src/bin/bevy_enchanced_input_settings_demo.rs
@@ -14,6 +14,7 @@ use bevy_drone_sim::free_camera_plugin::FreeCameraPlugin;
 use bevy_enhanced_input::prelude::*;
 use bevy_inspector_egui::bevy_egui::EguiPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use std::ops::Deref;
 
 fn main() {
     App::new()
@@ -32,10 +33,112 @@ fn main() {
         // Switching contexts
         .add_observer(switch_to_menu)
         .add_observer(switch_to_game)
+        // State management
+        .init_state::<GameState>()
         // Startup systems
-        .add_systems(Startup, (spawn_ui, setup))
-        .add_systems(Update, update_ui)
+        .add_systems(Startup, (spawn_debug_ui, setup))
+        .add_systems(Update, (update_debug_ui, button_coloring_system))
+        .add_systems(OnEnter(GameState::InMenu), spawn_menu)
+        .add_systems(OnExit(GameState::InMenu), despawn_menu)
         .run();
+}
+
+#[derive(Default, States, Debug, Eq, PartialEq, Hash, Clone, Copy)]
+enum GameState {
+    #[default]
+    InGame,
+    InMenu,
+}
+
+#[derive(Component)]
+struct Menu;
+
+const BUTTON_BACKGROUND_COLOR: Color = Color::srgba(0.2, 0.2, 0.2, 0.8);
+
+fn spawn_menu(mut commands: Commands) {
+    info!("Spawning menu UI");
+    // Spawn a menu UI
+    commands
+        .spawn((
+            Menu,
+            Name::new("Menu"),
+            Node {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                flex_direction: FlexDirection::Column,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BackgroundColor(Srgba::new(0.0, 0.0, 0.0, 0.7).into()),
+        ))
+        .with_children(|parent| {
+            parent
+                .spawn((
+                    Name::new("Back to Game Button"),
+                    Node {
+                        border: UiRect::all(Val::Px(2.0)),
+                        padding: UiRect::all(Val::Px(10.0)),
+                        ..default()
+                    },
+                    Button,
+                    BackgroundColor(BUTTON_BACKGROUND_COLOR),
+                    BorderRadius::all(Val::Px(2.0)),
+                    BorderColor(Srgba::new(1.0, 0.0, 0.0, 0.5).into()),
+                    Text::new("Back to Game"),
+                ))
+                .observe(
+                    |mut trigger: Trigger<Pointer<Click>>,
+                     mut commands: Commands,
+                     controller: Query<Entity, With<Controller>>| {
+                        info!(
+                            "'Back to Game' button was clicked (entity: {:?})",
+                            trigger.target()
+                        );
+                        // Stop the event from bubbling up the entity hierarchy
+                        trigger.propagate(false);
+
+                        commands.trigger_targets(
+                            Started::<IntoGameContext> {
+                                value: true,
+                                state: ActionState::Fired,
+                            },
+                            controller.single().unwrap(),
+                        );
+                    },
+                );
+        });
+}
+
+fn button_coloring_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                color.0 = Srgba::new(0.8, 0.8, 0.8, 0.8).into();
+            }
+            Interaction::Hovered => {
+                color.0 = Srgba::new(0.5, 0.5, 0.5, 0.8).into();
+            }
+            Interaction::None => {
+                color.0 = BUTTON_BACKGROUND_COLOR;
+            }
+        }
+    }
+}
+
+fn despawn_menu(mut commands: Commands, menu_query: Query<Entity, With<Menu>>) {
+    info!("Despawning menu UI");
+    // Despawn the menu UI
+    if let Ok(menu_entity) = menu_query.single() {
+        commands.entity(menu_entity).despawn();
+    } else {
+        warn!("No menu entity found to despawn.");
+    }
 }
 
 fn setup(mut commands: Commands) {
@@ -131,27 +234,51 @@ fn move_square(
     transform.translation += rotation * movement
 }
 
-fn switch_to_menu(trigger: Trigger<Started<IntoMenuContext>>, mut commands: Commands) {
+fn switch_to_menu(
+    trigger: Trigger<Started<IntoMenuContext>>,
+    mut commands: Commands,
+    state: Res<State<GameState>>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
     info!("Switching to menu context: {:#?}", trigger);
     commands.entity(trigger.target()).insert((
         ContextActivity::<InGameContext>::INACTIVE,
         ContextActivity::<InMenuContext>::ACTIVE,
     ));
+    if state.deref() == &GameState::InGame {
+        next_state.set(GameState::InMenu);
+        info!("Changed state to InMenu");
+    } else {
+        warn!("Tried to switch to Menu context, but current state is not InGame");
+    }
 }
 
 fn log_move_in_menu(trigger: Trigger<Fired<MoveInMenu>>) {
     info!("Moving in menu: {:#?}", trigger);
 }
 
-fn switch_to_game(trigger: Trigger<Started<IntoGameContext>>, mut commands: Commands) {
+// Change state to InGame when IntoGameContext action is triggered
+fn switch_to_game(
+    trigger: Trigger<Started<IntoGameContext>>,
+    mut commands: Commands,
+    state: Res<State<GameState>>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
     info!("Switching to Game context: {:#?}", trigger);
     commands.entity(trigger.target()).insert((
         ContextActivity::<InMenuContext>::INACTIVE,
         ContextActivity::<InGameContext>::ACTIVE,
     ));
+
+    if state.deref() == &GameState::InMenu {
+        next_state.set(GameState::InGame);
+        info!("Changed state to InGame");
+    } else {
+        warn!("Tried to switch to Game context, but current state is not InMenu");
+    }
 }
 
-fn spawn_ui(mut commands: Commands) {
+fn spawn_debug_ui(mut commands: Commands) {
     commands.spawn((
         Node {
             width: Val::Percent(100.0),
@@ -176,7 +303,7 @@ fn spawn_ui(mut commands: Commands) {
 }
 
 /// Prints active context
-fn update_ui(
+fn update_debug_ui(
     mut text_to_change: Query<&mut Text, With<ActiveContextUiMarker>>,
     contexts: Query<
         (
